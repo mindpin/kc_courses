d79419471fccf90bbec529390ff8edcc0f189c3a
destinyd <destinyd_war@163.com>
2015-08-24 13:51:38 +0800

    课件前端测试

---
+++ sample/spec/features/wares_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+feature "课件页面" do
+  background do
+    @course = KcCourses::Course.create(:title => '测试课程', :desc => '课程描述')
+    @chapter = @course.chapters.create(:title => '测试章节', :desc => '章节描述')
+  end
+
+  scenario "访问章节详情页" do
+    visit "/chapters/#{@chapter.id}"
+    expect(page).to have_content @chapter.title
+    expect(page).to have_content @chapter.desc
+  end
+
+  scenario "访问章节详情页转入上传课件" do
+    visit "/chapters/#{@chapter.id}"
+    click_link '上传课件'
+    expect(find('h1')).to have_content('上传课件')
+    expect(current_path).to eq("/chapters/#{@chapter.id}/wares/new")
+  end
+
+  scenario "上传课件, 正常提交" do
+    visit "/chapters/#{@chapter.id}/wares/new"
+    within("#new_ware") do
+      fill_in 'ware_title', :with => '课件标题'
+      fill_in 'ware_desc', :with =>  '课件描述'
+    end
+    click_button '提交'
+    expect(page).to have_content('修改章节信息')
+    @create_ware = KcCourses::Ware.last
+    expect(page).to have_content(@create_ware.title)
+    expect(current_path).to eq("/chapters/#{@chapter.id}")
+  end
+
+  scenario "列表跳转进入编辑" do
+    other_ware = @chapter.wares.create(:title => '其他标题', :desc => '其他描述')
+    visit "/chapters/#{@chapter.id}"
+    click_link '编辑'
+    expect(find('h1')).to have_content('修改课件')
+    expect(current_path).to eq("/wares/#{other_ware.id}/edit")
+  end
+
+  scenario "修改课件, 正常提交" do
+    other_ware = @chapter.wares.create(:title => '其他标题', :desc => '其他描述')
+    visit "/wares/#{other_ware.id}/edit"
+    expect(find('h1')).to have_content('修改课件')
+    within(".edit_ware") do
+      fill_in 'ware_title', :with => '其他标题改'
+      fill_in 'ware_desc', :with =>  '其他描述改'
+    end
+    click_button '提交'
+    expect(page).to have_content('其他标题改')
+    expect(current_path).to eq("/chapters/#{@chapter.id}")
+  end
+
+  scenario "列表删除" do
+    other_ware = @chapter.wares.create(:title => '其他标题', :desc => '其他描述')
+    visit "/chapters/#{@chapter.id}"
+    expect(page).to have_content(other_ware.title)
+    # 默认确认
+    #accept_confirm do
+      click_link '删除'
+    #end
+    expect(page).not_to have_content(other_ware.title)
+    expect(current_path).to eq("/chapters/#{@chapter.id}")
+  end
+end
