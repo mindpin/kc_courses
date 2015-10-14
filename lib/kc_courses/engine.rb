f11c4bf31e4717b7b70d9826861e29783a2f6bdc
fushang318 <fushang318@gmail.com>
2015-10-14 15:08:08 +0800

    修改 user 关联模型方法声明方式

--- lib/kc_courses/engine.rb
+++ lib/kc_courses/engine.rb
@@ -3,6 +3,14 @@ module KcCourses
     isolate_namespace KcCourses
     config.to_prepare do
       ApplicationController.helper ::ApplicationHelper
+
+      User.class_eval do
+        has_many :courses, class_name: 'KcCourses::Course'
+        has_many :chapters, class_name: 'KcCourses::Chapter'
+        has_many :wares, class_name: 'KcCourses::Ware'
+        has_many :joins, class_name: 'KcCourses::Join'
+      end
     end
+
   end
 end
