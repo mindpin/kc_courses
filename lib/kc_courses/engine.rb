ecbb6e346a83d0e61b5be30b141aef5948cad5d9
destinyd <destinyd_war@163.com>
2015-10-14 17:48:03 +0800

    添加user.join_course user.cancel_join_course 方法

--- lib/kc_courses/engine.rb
+++ lib/kc_courses/engine.rb
@@ -9,6 +9,17 @@ module KcCourses
         has_many :chapters, class_name: 'KcCourses::Chapter'
         has_many :wares, class_name: 'KcCourses::Ware'
         has_many :join_courses, class_name: 'KcCourses::JoinCourse'
+
+        define_method :join_course do |course|
+          return if course.class.name != 'KcCourses::Course'
+          unless self.join_courses.where(course: course).any?
+            self.join_courses.create course: course
+          end
+        end
+
+        define_method :cancel_join_course do |course|
+          join_courses.where(course: course).destroy_all > 0 ? true : false
+        end
       end
     end
 
--- sample/spec/models/user_spec.rb
+++ sample/spec/models/user_spec.rb
@@ -6,4 +6,27 @@ RSpec.describe User, type: :model do
       expect(create(:user).name).to match(/user\d+/)
     }
   end
+
+  describe "join_course" do
+    before do
+      @user = create(:user)
+      @course = create(:course)
+    end
+
+    it '#join_course' do
+      expect(@user.join_course(nil)).to be_nil
+      expect(@user.join_course('')).to be_nil
+      expect(@user.join_course(@course)).not_to be_nil
+      expect(@user.join_courses.count).to eq(1)
+      expect(@user.join_course(@course)).to be_nil
+    end
+
+    it '#cancel_join_course' do
+      expect(@user.cancel_join_course(@course)).to eq(false)
+      @user.join_course(@course)
+      expect(@user.join_courses.count).to eq(1)
+      expect(@user.cancel_join_course(@course)).to eq(true)
+      expect(@user.join_courses.count).to eq(0)
+    end
+  end
 end
