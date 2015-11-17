8d62fdc57949ed00d3588d231ac141af1d828e98
hengtangan2025 <504371515@qq.com>
2015-11-17 18:39:39 +0800

    .distinct方法

--- sample/spec/models/kc_courses/ware_reading_spec.rb
+++ sample/spec/models/kc_courses/ware_reading_spec.rb
@@ -90,7 +90,6 @@ RSpec.describe KcCourses::WareReading, type: :model do
       ware_reading4 = @course4.set_read_percent_by_user(@user2, 100)
       ware_reading5 = @course5.set_read_percent_by_user(@user2, 100)
       ware_reading7 = @course1.set_read_percent_by_user(@user2, 88)
-      p KcCourses::Course.studing_of_user(@user2)
       expect(KcCourses::Course.studing_of_user(@user2)).to eq([@course1,@course2])
       expect(KcCourses::Course.studied_of_user(@user2)).to eq([@course3,@course4,@course5])
     }
