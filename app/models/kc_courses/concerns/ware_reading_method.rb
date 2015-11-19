2082c876d53dd6402973f036953db77f11b95355
hengtangan2025 <504371515@qq.com>
2015-11-19 12:15:13 +0800

    1

--- app/models/kc_courses/concerns/ware_reading_method.rb
+++ app/models/kc_courses/concerns/ware_reading_method.rb
@@ -2,14 +2,14 @@ module KcCourses
   module Concerns
     module WareReadingMethod
       extend ActiveSupport::Concern
-      
+
       # 设置 user 已经完成了 course/chapter/ware 百分之 read_percent 的内容
       def set_read_percent_by_user(user, read_percent)
         ware_readings.create(:creator => user, :read_percent => read_percent)
       end
       # 设置 user 在某一天内 course/chapter/ware 的学习进度变化
       def set_read_percent_change_by_user(user, read_percent_change, time)
-        if ware_reading_deltas.where(:creator_id => user.id.to_s).count != 0 
+        if ware_reading_deltas.where(:creator_id => user.id.to_s).count != 0
           read_percent_before = 0
           ware_reading_deltas.map do |ware_reading_delta|
             if ware_reading_delta.time < time
@@ -23,7 +23,7 @@ module KcCourses
           return ware_reading_deltas.create(:creator => user, :read_percent_change => read_percent_change, :time => time, :read_percent => read_percent_change)
         end
       end
-      
+
       # user 是否已经完成整个 course/chapter/ware 的学习（read_percent 是 100 时，表示完成学习）
       def has_read_by_user?(user)
         if ware_readings.where(:creator_id => user.id.to_s).first.read_percent == 100
@@ -35,17 +35,17 @@ module KcCourses
 
       # user 已经学习了 course/chapter/ware 多少百分比的内容，返回值是代表百分比的数字
       def read_percent_of_user(user)
-        return ware_readings.where(:creator_id => user.id.to_s).first.read_percent
+        return ware_readings.where(:creator_id => user.id.to_s).first.try(:read_percent) || 0
       end
 
       # user 某一天内学习了 course/chapter/ware 多少百分比的内容，返回值是代表百分比的数字
       def read_percent_change_of_user(user, time)
         return ware_reading_deltas.where(:creator_id => user.id.to_s, :time => time).first.read_percent_change
       end
-      
+
       # user 截止到某天（包括这个某天内学习的），已经学习了 course/chapter/ware 多少百分比的内容，返回值是代表百分比的数字
       def read_percent_of_user_before_time(user, time)
-        return ware_reading_deltas.where(:creator_id => user.id.to_s, :time => time).first.read_percent
+        return ware_reading_deltas.where(:creator_id => user.id.to_s, :time.lt => time.en:time.lte => timeread_percent
       end
     end
   end
