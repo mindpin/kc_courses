62e5d52ef2c12aae61e21eebdf80f91465fef0b0
hengtangan2025 <504371515@qq.com>
2015-12-03 09:32:00 +0800

    关于issue26的修改

--- app/controllers/kc_courses/api/courses_controller.rb
+++ app/controllers/kc_courses/api/courses_controller.rb
@@ -4,25 +4,21 @@ module KcCourses
       user = current_user
       course = KcCourses::Course.find(params[:id])
       render :json => {
-        :id => course.id.to_s
-        :currnet_ware_id => course.studing_ware_of_user(user).id.to_s
-        :percent => course.read_percent_of_user(user)
-        :chapters => [
-          course.chapters.each do |chapter|
-            {
-              :id => chapter.id.to_s
-              :percent => chapter.read_percent_of_user(user)
-              :wares => [
-                chapter.wares.each do |ware|
-                  {
-                    :id => ware.id.to_s
-                    :percent => ware.read_percent_of_user(user)
-                  }
-                end
-              ]
-            }
-          end
-        ]
+        :id => course.id.to_s,
+        :currnet_ware_id => course.studing_ware_of_user(user).id.to_s,
+        :percent => course.read_percent_of_user(user),
+        :chapters => course.chapters.map do |chapter|
+          {
+            :id => chapter.id.to_s,
+            :percent => chapter.read_percent_of_user(user),
+            :wares => chapter.wares.map do |ware|
+              {
+                :id => ware.id.to_s,
+                :percent => ware.read_percent_of_user(user)
+              }
+            end
+          }
+        end
       }
     end
   end
