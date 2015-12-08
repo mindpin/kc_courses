396576f6e37d0670b1205c0146730913e5bba25b
hengtangan2025 <504371515@qq.com>
2015-12-08 16:58:35 +0800

    设置学习状态

--- app/controllers/kc_courses/api/wares_controller.rb
+++ app/controllers/kc_courses/api/wares_controller.rb
@@ -3,7 +3,9 @@ module KcCourses
     class WaresController < KcCourses::ApplicationController
       def study
         user = current_user
-        ware = KcCourses::Ware.find(params[:id])
+        course = KcCourses::Course.find(params[:course_id])
+        ware = course.studing_ware_of_user(user)
+        ware.set_read_percent_by_user(user, params[:percent])
         render :json => {
           :id => ware.id.to_s,
           :percent => ware.read_percent_of_user(user)
