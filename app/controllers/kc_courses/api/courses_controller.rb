7f6615299c7f5cc2cd4cdd11a415c75a31706add
hengtangan2025 <504371515@qq.com>
2015-12-23 16:24:43 +0800

    新学习进度读取API

--- app/controllers/kc_courses/api/courses_controller.rb
+++ app/controllers/kc_courses/api/courses_controller.rb
@@ -6,20 +6,32 @@ module KcCourses
         course = KcCourses::Course.find(params[:id])
         render :json => {
           :id => course.id.to_s,
-          :current_ware_id => 
-            if course.studing_ware_of_user(user) == nil
-              nil
-            else
-              course.studing_ware_of_user(user).id.to_s
-            end,
+          :spent_time => course.spent_time_of_user(user),
+          :title => course.title,
+          :current_ware => {
+            :id => 
+              if course.studing_ware_of_user(user) == nil
+                nil
+              else
+                course.studing_ware_of_user(user).id.to_s
+              end,
+            :title => 
+              if course.studing_ware_of_user(user) == nil
+                nil
+              else
+                course.studing_ware_of_user(user).title
+              end
+          },
           :percent => course.read_percent_of_user(user),
           :chapters => course.chapters.map do |chapter|
             {
               :id => chapter.id.to_s,
+              :title => chapter.title,
               :percent => chapter.read_percent_of_user(user),
               :wares => chapter.wares.map do |ware|
                 {
                   :id => ware.id.to_s,
+                  :title => ware.title,
                   :percent => ware.read_percent_of_user(user)
                 }
               end
