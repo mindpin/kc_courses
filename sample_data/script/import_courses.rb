d108792e7703d749d552fca285ce20e8d076ccff
hengtangan2025 <504371515@qq.com>
2016-01-04 17:24:32 +0800

    添加三个课程

--- sample_data/script/import_courses.rb
+++ sample_data/script/import_courses.rb
@@ -3,11 +3,11 @@ module KcCourses
     def self.import
       json_courses = IO.read(File.expand_path("../../data/courses.json",__FILE__))
       courses_hash = JSON.parse(json_courses)
-      creator = User.create!(:id => "564165656e95521109000001", :email => "123456@qq.com", :password => "123456", :name => "1234")
-      reader = User.create!(:id => "564165656e95521109000002", :email => "123467@qq.com", :password => "1234567", :name => "123")
+      creator = User.create(:id => "564165656e95521109000001", :email => "123456@qq.com", :password => "123456", :name => "1234")
+      reader = User.create(:id => "564165656e95521109000002", :email => "123467@qq.com", :password => "1234567", :name => "123")
       courses = courses_hash["courses"]
       courses.each do |course|
-        KcCourses::Course.create!(:id => course["course_id"], :title => course["title"], :user_id => course["creator_id"])
+        KcCourses::Course.create(:id => course["course_id"], :title => course["title"], :user_id => course["creator_id"])
         chapters = course["chapters"]
         chapters.each do |chapter|
           KcCourses::Chapter.create(:id => chapter["chapter_id"], :title => chapter["title"], :course_id => chapter["course_id"], :user_id => chapter["creator_id"])
