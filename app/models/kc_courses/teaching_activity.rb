fe2007f854a5bd0fb933aca2c0eaecda9b35f3ad
destinyd <destinyd_war@163.com>
2016-04-07 15:30:44 +0800

    教学活动时间、申请时间，以及相关方法

--- app/models/kc_courses/teaching_activity.rb
+++ app/models/kc_courses/teaching_activity.rb
@@ -8,11 +8,40 @@ module KcCourses
     field :desc, type: String
     field :started_at, type: Time
     field :ended_at, type: Time
+    # 报名
+    field :apply_started_at, type: Time
+    field :apply_ended_at, type: Time
 
     belongs_to :manager, class_name: 'User'#, inverse_of: 
 
     has_many :events, class_name: 'KcCourses::TeachingEvent', inverse_of: :activity
 
     validates :name, presence: true
+
+    def started?
+      started_at.nil? || Time.now >= started_at
+    end
+
+    def ended?
+      return false if ended_at.nil?
+      Time.now >= ended_at
+    end
+
+    def running?
+      started? and !ended?
+    end
+
+    def apply_started?
+      apply_started_at.nil? || Time.now >= apply_started_at
+    end
+
+    def apply_ended?
+      return false if apply_ended_at.nil?
+      Time.now >= apply_ended_at
+    end
+
+    def could_apply?
+      apply_started? and !apply_ended?
+    end
   end
 end
--- sample/spec/models/kc_courses/teaching_activity_spec.rb
+++ sample/spec/models/kc_courses/teaching_activity_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe KcCourses::TeachingActivity, type: :model do
     expect(@teaching_activity.respond_to?(:desc)).to be true
     expect(@teaching_activity.respond_to?(:started_at)).to be true
     expect(@teaching_activity.respond_to?(:ended_at)).to be true
+    expect(@teaching_activity.respond_to?(:apply_started_at)).to be true
+    expect(@teaching_activity.respond_to?(:apply_ended_at)).to be true
 
     expect(@teaching_activity.respond_to?(:manager_id)).to be true
   end
@@ -21,5 +23,70 @@ RSpec.describe KcCourses::TeachingActivity, type: :model do
   end
 
   describe "方法" do
+    it "started?" do
+      expect(create(:teaching_activity, started_at: nil).started?).to be true
+      expect(create(:teaching_activity, started_at: 1.day.ago).started?).to be true
+      expect(create(:teaching_activity, started_at: Time.now).started?).to be true
+
+      expect(create(:teaching_activity, started_at: 1.day.from_now).started?).to be false
+    end
+
+    it "ended?" do
+      expect(create(:teaching_activity, ended_at: nil).ended?).to be false
+      expect(create(:teaching_activity, ended_at: 1.day.from_now).ended?).to be false
+
+      expect(create(:teaching_activity, ended_at: 1.day.ago).ended?).to be true
+      expect(create(:teaching_activity, ended_at: Time.now).ended?).to be true
+    end
+
+    it "running?" do
+      expect(create(:teaching_activity, started_at: nil, ended_at: nil).running?).to be true
+      expect(create(:teaching_activity, started_at: 1.day.ago, ended_at: nil).running?).to be true
+      expect(create(:teaching_activity, started_at: Time.now, ended_at: nil).running?).to be true
+      expect(create(:teaching_activity, started_at: 1.day.from_now, ended_at: nil).running?).to be false
+
+      expect(create(:teaching_activity, started_at: nil, ended_at: 1.day.ago).running?).to be false
+      expect(create(:teaching_activity, started_at: nil, ended_at: Time.now).running?).to be false
+      expect(create(:teaching_activity, started_at: nil, ended_at: 1.day.from_now).running?).to be true
+
+      expect(create(:teaching_activity, started_at: 1.day.ago, ended_at: 1.day.from_now).running?).to be true
+      expect(create(:teaching_activity, started_at: 1.day.ago, ended_at: 1.day.ago).running?).to be false
+      expect(create(:teaching_activity, started_at: 1.day.from_now, ended_at: 1.day.from_now).running?).to be false
+
+      expect(create(:teaching_activity, started_at: 1.day.from_now, ended_at: 1.day.ago).running?).to be false
+    end
+
+    it "apply_started?" do
+      expect(create(:teaching_activity, apply_started_at: nil).apply_started?).to be true
+      expect(create(:teaching_activity, apply_started_at: 1.day.ago).apply_started?).to be true
+      expect(create(:teaching_activity, apply_started_at: Time.now).apply_started?).to be true
+
+      expect(create(:teaching_activity, apply_started_at: 1.day.from_now).apply_started?).to be false
+    end
+
+    it "apply_ended?" do
+      expect(create(:teaching_activity, apply_ended_at: nil).apply_ended?).to be false
+      expect(create(:teaching_activity, apply_ended_at: 1.day.from_now).apply_ended?).to be false
+
+      expect(create(:teaching_activity, apply_ended_at: 1.day.ago).apply_ended?).to be true
+      expect(create(:teaching_activity, apply_ended_at: Time.now).apply_ended?).to be true
+    end
+
+    it "could_apply?" do
+      expect(create(:teaching_activity, apply_started_at: nil, apply_ended_at: nil).could_apply?).to be true
+      expect(create(:teaching_activity, apply_started_at: 1.day.ago, apply_ended_at: nil).could_apply?).to be true
+      expect(create(:teaching_activity, apply_started_at: Time.now, apply_ended_at: nil).could_apply?).to be true
+      expect(create(:teaching_activity, apply_started_at: 1.day.from_now, apply_ended_at: nil).could_apply?).to be false
+
+      expect(create(:teaching_activity, apply_started_at: nil, apply_ended_at: 1.day.ago).could_apply?).to be false
+      expect(create(:teaching_activity, apply_started_at: nil, apply_ended_at: Time.now).could_apply?).to be false
+      expect(create(:teaching_activity, apply_started_at: nil, apply_ended_at: 1.day.from_now).could_apply?).to be true
+
+      expect(create(:teaching_activity, apply_started_at: 1.day.ago, apply_ended_at: 1.day.from_now).could_apply?).to be true
+      expect(create(:teaching_activity, apply_started_at: 1.day.ago, apply_ended_at: 1.day.ago).could_apply?).to be false
+      expect(create(:teaching_activity, apply_started_at: 1.day.from_now, apply_ended_at: 1.day.from_now).could_apply?).to be false
+
+      expect(create(:teaching_activity, apply_started_at: 1.day.from_now, apply_ended_at: 1.day.ago).could_apply?).to be false
+    end
   end
 end
